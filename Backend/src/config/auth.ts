f1fd8742e537388d6ee2871c9c0091267e555a97
Nickolas <54966916+NickRios@users.noreply.github.com>
2021-06-01 11:54:10 -0300

    Delete Backend directory

--- Backend/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    jwt: {
-        secret: 'a2591bc5dc93b8ab1404763abea518dc',
-        expiresIn: '1d',
-    },
-}
--- Backend/src/modules/users/repositories/IUserTokensRepository.ts
+++
@@ -1,6 +0,0 @@
-import UserToken from "../model/UserToken"
-
-export default interface IUserTokensRepository {
-  generate(user_id: string): Promise<UserToken>;
-  findByToken(token: string): Promise<UserToken | undefined>;
-}
